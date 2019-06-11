1c750235387fca46d06d9fc749c1581a0601b627
Arvin Agas <arvagas@gmail.com>
2019-06-11 18:15:57 -0400

    Completed challenge 1 for closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const animal = "Fox"
+function animalSpeaks() {
+  return animal
+}
+console.log(animalSpeaks())
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
